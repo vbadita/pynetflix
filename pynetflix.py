53dbda8be4ee5a1e1c0917ad2f5f793fafa0feae
nathan <nathan@hotmail.com>
2020-05-23 21:39:02 +0100

    fix for banner prompt in firefox at homepage, duplicate csv's created commented out duplicate section

--- pynetflix.py
+++ pynetflix.py
@@ -1,9 +1,12 @@
 from selenium import webdriver
 import os
 import csv
+import time
 
 username = os.environ['NETFLIX_EMAIL']
 password = os.environ['NETFLIX_PASSWORD']
+account = os.environ['NETFLIX_ACCOUNT']
+
 
 profile = webdriver.FirefoxProfile()
 #profile.set_preference('browser.download.folderList', 2) # custom location
@@ -15,10 +18,16 @@ driver = webdriver.Firefox(profile)
 netflix_page = "https://netflix.com"
 netflix_activity_page = "https://www.netflix.com/viewingactivity"
 
+
 def connect(username,password):
-    driver.implicitly_wait(3)
+    driver.implicitly_wait(5)
     driver.get(netflix_page)
-    menu =  driver.find_element_by_class_name('authLinks')
+    driver.implicitly_wait(2)
+    # Closes cookie banner
+    cookie_bar = driver.find_element_by_class_name('close-button')
+    cookie_bar.click()
+    driver.implicitly_wait(2)
+    menu = driver.find_element_by_class_name('authLinks')
     actions = webdriver.ActionChains(driver)
     actions.move_to_element(menu)
     actions.click(menu)
@@ -31,16 +40,18 @@ def connect(username,password):
     login_button.click()
     return driver
 
+
 def get_history(profile):
     profile_to_click = driver.find_element_by_xpath(f"//span[@class='profile-name' and contains(text(),'{profile}')]")
     profile_to_click.click()
     driver.get(netflix_activity_page)
     activity_button = driver.find_element_by_class_name("viewing-activity-footer-download")
     activity_button.click()
-    download_page = driver.find_element_by_class_name("modal-action-button")
-    download_page.click()
+    # Below code not needed - downloads file twice in case needed later
+    # download_page = driver.find_element_by_class_name("modal-action-button")
+    # download_page.click()
 
 
 if __name__ == '__main__':
     netflix_connected = connect(username,password)
-    get_history("valentin")
+    get_history(account)
